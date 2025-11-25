a5cbb1ce6f09c82dbed96299119b34dc892cbaf5
Mitja Jecelj <mitjajecelj@Mitjas-MacBook-Pro.local>
2025-11-25 11:02:58 +0100

    Test: Add 800 FLY import test

--- app/Console/Commands/TestGaleonImport.php
+++ app/Console/Commands/TestGaleonImport.php
@@ -22,51 +22,48 @@ class TestGaleonImport extends Command
     {
         $this->info('Testing Galeon yacht import...');
 
-        // Mock data for yacht 440 FLY - WITH REAL GALLERY URLS
+        // Real data for yacht 800 FLY (Post ID 907) - LIMITED GALLERIES FOR TEST
         $mockData = [
-            'source_post_id' => 837,
-            'name' => '440 FLY Real Test',
-            'slug' => '440-fly-real-galleries',
+            'source_post_id' => 907,
+            'name' => '800 FLY',
+            'slug' => '800-fly-test',
             'state' => 'new',
             'brand' => 'Galeon',
             'model' => 'Flybridge',
             'fields' => [
-                'sub_titile' => '<p>An exciting endeavor</p>',
-                'full_description' => '<div class="row"><p>The sleek exterior marks another collaboration...</p></div>',
-                'specifications' => '<table class="yacht-tech-table"><tbody><tr><td>Length overall</td><td>13,97 m</td></tr></tbody></table>',
-                'lenght' => 13.97,
+                'sub_titile' => '<p>The Flagship</p>',
+                'full_description' => '<div class="row"><div class="col-5 col-push-1 text-first"><p class="p1">Thanks to many years of experience, passed down in our company from generation to generation, we have created the Galeon 800 FLY, which combines a sporty silhouette and impressive performance with a sense of space and luxury previously reserved only for megayachts.</p></div></div>',
+                'specifications' => '<table class="yacht-tech-table"><tbody><tr><td class="name">Length overall</td><td class="unit-si">[m]</td><td class="value-si">25,35</td><td class="unit-imperial">[ft]</td><td class="value-imperial">83\'2"</td></tr></tbody></table>',
+                'lenght' => 25.35,
             ],
             'media' => [
-                'cover_image' => 'https://galeonadriatic.com/wp-content/uploads/2025/06/DJI_0311.webp',
-                'grid_image' => 'https://galeonadriatic.com/wp-content/uploads/2025/06/DJI_0322.webp',
-                'grid_image_hover' => 'https://galeonadriatic.com/wp-content/uploads/2025/06/DJI_0310.webp',
-                'pdf_brochure' => 'https://galeonadriatic.com/wp-content/uploads/2025/06/Broszura-Galeon_440-5-1.pdf',
+                'cover_image' => 'https://galeonadriatic.com/wp-content/uploads/2025/06/DJI_0961.webp',
+                'grid_image' => 'https://galeonadriatic.com/wp-content/uploads/2025/06/DJI_0068.webp',
+                'grid_image_hover' => 'https://galeonadriatic.com/wp-content/uploads/2025/06/DJI_0046.webp',
+                'pdf_brochure' => 'https://galeonadriatic.com/wp-content/uploads/2025/06/Galeon-800fly-brochure-1.pdf',
                 'video_url' => 'https://www.youtube.com/watch?v=Scx',
                 'gallery_exterior' => [
-                    'https://galeonadriatic.com/wp-content/uploads/slider58/DJI_0336.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/slider58/DJI_0413.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/slider58/DJI_0398.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/slider58/DJI_0446.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/slider58/DJI_0449.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/slider58/DJI_0455.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/slider58/DJI_0322.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/slider58/DJI_0344.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/slider36/DJI_0025.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/slider36/DJI_0023.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/slider36/DJI_0046.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/slider36/DJI_0068.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/slider36/DJI_0100.webp',
                 ],
                 'gallery_interrior' => [
-                    'https://galeonadriatic.com/wp-content/uploads/slider60/_MG_6203-HDR.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/slider60/_MG_6197-HDR.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/slider60/_MG_6212-HDR.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/slider60/_MG_6209-HDR.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/slider38/_MG_6477-HDR.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/slider38/_MG_6480-HDR.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/slider38/_MG_6497-HDR.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/slider38/_MG_6510-HDR.webp',
                 ],
                 'gallery_cockpit' => [
-                    'https://galeonadriatic.com/wp-content/uploads/slider59/_MG_6260.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/slider59/_MG_6263.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/slider59/_MG_6262.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/slider37/_MG_4280.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/slider37/_MG_4305.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/slider37/_MG_4267.webp',
                 ],
                 'gallery_layout' => [
-                    'https://galeonadriatic.com/wp-content/uploads/2025/06/494-Galeon-440FLY-Coloured-GA-Fly-Deck-14.10.2022.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/2025/06/494-Galeon-440FLY-Coloured-GA-Main-Deck-14.10.2022.webp',
-                    'https://galeonadriatic.com/wp-content/uploads/2025/06/494-Galeon-440FLY-Coloured-GA-3-Cabin-Std-V2-14.10.2022.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/2025/06/498_Galeon-800-Fly-_Marketing-GA-01_2020-01-08_Upper-Deck-v2-HIGH-RES__MODYFIKACJA.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/2025/06/498_Galeon-800-Fly-_Marketing-GA-01_2020-01-08_Upper-Deck-HIGH-RES__MODYFIKACJA.webp',
+                    'https://galeonadriatic.com/wp-content/uploads/2025/06/498_Galeon-800-Fly-_Marketing-GA-01_2020-01-08_Main-Deck-HIGH-RES__MODYFIKACJA.webp',
                 ],
             ],
             'skipped_fields' => [],
@@ -76,6 +73,7 @@ class TestGaleonImport extends Command
         $this->line('  Name: ' . $mockData['name']);
         $this->line('  Model: ' . $mockData['model']);
         $this->line('  Length: ' . $mockData['fields']['lenght'] . ' m');
+        $this->line('  Galleries: ' . (count($mockData['media']['gallery_exterior']) + count($mockData['media']['gallery_interrior']) + count($mockData['media']['gallery_cockpit']) + count($mockData['media']['gallery_layout'])) . ' images (limited for test)');
 
         $this->newLine();
         $this->info('Starting import...');
